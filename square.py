4a3fbf40d331cac88e606bc983a1efefe6022376
dhruva7vernekar-blip <dhruva77vernekar@gmail.com>
2026-01-17 11:41:56 +0530

    save

---
+++ square.py
@@ -0,0 +1,6 @@
+def square(num):
+    return num * num
+
+if __name__ == "__main__":
+    num = 5
+    print(f"Square of {num} is: {square(num)}")
---
+++ test_square.py
@@ -0,0 +1,4 @@
+from square import square
+
+def test_case():
+    assert test_case(5)==25
